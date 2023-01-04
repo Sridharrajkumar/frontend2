0c891a543f4690acc62d112f01acb427660dd569
Sridharrajkumar <sridharrajkumar47@gmail.com>
2023-01-04 19:56:46 +0530

    first coomit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return( <div>
+        <div><h2>Expense Item!</h2></div>
+        <div>food Rs-10</div>
+        <div>Petrol Rs-100</div>
+        <div>Movies Rs-200</div>
+    </div>);
+    
+}
+
+export default ExpenseItem;
