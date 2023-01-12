edb34b92672e75a031cb9b52cf0d3fe71c016ed5
Sridharrajkumar <sridharrajkumar47@gmail.com>
2023-01-12 17:25:39 +0530

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
-function ExpenseItem(){
-    return( <div>
-        <div><h2>Expense Item!</h2></div>
-        <div>food Rs-10</div>
-        <div>Petrol Rs-100</div>
-        <div>Movies Rs-200</div>
-    </div>);
+import './ExpenseItem.css'
+
+function ExpenseItem(props){
+   
+    return( 
+        <div  className='expense-item'>
+          <div >{props.date.toISOString()}</div>
+             <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='Location_Of_Expenses'>{props.location}</div>
+            <div className='expense-item__price'>{props.amount}</div>
+        </div>
+        
+    </div>
+    );
     
 }
 
