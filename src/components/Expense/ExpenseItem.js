e6e863b59a656164138788a5faeee741b21a5b17
Sridharrajkumar <sridharrajkumar47@gmail.com>
2023-02-14 23:26:12 +0530

    STATE

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './ExpenseItem.css'
 import ExpensesDate from './ExpensesDate';
 import ExpensesDetails from './ExpensesDetails';
-//import '../UI/Card.css'
+import '../UI/Card.css'
 
 
-function ExpenseItem(props){
-   
+function ExpenseItem(props) {
+    
+
     return( 
        
           <div className='expense-item card'>
           <ExpensesDate  date={props.date}/>
           <ExpensesDetails amount={props.amount} title={props.title}/> 
-           <button>Change Title</button>
+           
         </div>
         
     );
