b7bd4501ea9cb11bf74ec2024468de5959bcf731
Sridharrajkumar <sridharrajkumar47@gmail.com>
2023-02-14 23:26:26 +0530

    STATE

--- src/components/Expense/ExpensesDetails.js
+++ src/components/Expense/ExpensesDetails.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 function ExpensesDetails(props)
 {
+    const [title, setTitle]=useState(props.title)
+    
+    function ClickHandler(){
+        setTitle('updated');
+        console.log(title);
+    }
+
+    const [amount,setAmount]=useState(props.amount)
+    function amountChanger(){
+        setAmount('100');
+
+    }
+
     return (
         <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='Location_Of_Expenses'>{props.location}</div>
-        <div className='expense-item__price'>{props.amount}</div>
+        <div className='expense-item__price'>{amount}</div>
+        <button onClick={amountChanger}>change price</button>
+        <button onClick={ClickHandler}>Change Title</button>
         </div>
     );
 
