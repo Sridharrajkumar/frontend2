18cfa66c37b803fbc3dd7f84c5ab93f95a472769
Sridharrajkumar <sridharrajkumar47@gmail.com>
2023-02-16 16:47:17 +0530

    userdetailadded

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 
-import React from 'react';
+import React,{useState} from 'react';
 import Expenses from "./components/Expense/Expenses";
-import ExpensesForm from './components/ExpensesForm';
+import NewExpenses from './components/NewExpenses/NewExpenses';
+  
 
+const dummyexpenses=[
+  {id:'e1',title:'car insurance', amount:500.50 ,date: new Date(2023, 0, 6) , LocationOfExpenditure:'car showroom'},
+  {id:'e2',title:'bike insurance', amount:255.60 ,date: new Date(2023, 3, 6) , LocationOfExpenditure:'bike showroom'},
+  {id:'e3',title:'house insurance', amount:1050.70 ,date: new Date(2023, 6, 6) , LocationOfExpenditure:'house emi'},
+  {id:'e4',title:'hotel', amount:50.50 ,date: new Date(2023, 11, 6) , LocationOfExpenditure:'hotel restaurant'},
+   
+]
 
 function App() {
+  const[expenses,setExpenses]=useState(dummyexpenses)
 
-  const expenses=[
-    {title:'car insurance', amount:500.50 ,date: new Date(2023, 0, 6) , LocationOfExpenditure:'car showroom'},
-    {title:'bike insurance', amount:255.60 ,date: new Date(2023, 3, 6) , LocationOfExpenditure:'bike showroom'},
-    {title:'house insurance', amount:1050.70 ,date: new Date(2023, 6, 6) , LocationOfExpenditure:'house emi'},
-    {title:'hotel', amount:50.50 ,date: new Date(2023, 11, 6) , LocationOfExpenditure:'hotel restaurant'},
-     
-  ]
+  function addExpenseHandler(expense){
+    setExpenses((preExpenses) =>{
+      return [expense, ...preExpenses]
+
+    });
+  }
+
+
   return (
     <div>
 
-      <ExpensesForm />
+      <NewExpenses onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
       
     </div>
