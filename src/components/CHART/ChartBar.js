33b941331cda7e2ae961b39a2e4b4e3f6f9c661e
Sridharrajkumar <sridharrajkumar47@gmail.com>
2023-02-18 23:19:39 +0530

    chartbaradded

--- src/components/CHART/ChartBar.js
+++ src/components/CHART/ChartBar.js
@@ -4,9 +4,9 @@ import './ChartBar.css';
 
 const CharBar = (props) => {
     let barFillHeight= '0%';
-    if(props.max>0)
+    if(props.maxValue>0)
     {
-        barFillHeight = Math.round((props.value/props.maxValue)*100)+'%'
+        barFillHeight = Math.round((props.value/props.maxValue)*100)+'%';
     }
     return <div className='chart-bar'>
         <div className='chart-bar__inner'>
